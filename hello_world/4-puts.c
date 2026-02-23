bef47857a996dcc17eadcef224d94fd8910f2ea3
Nicolas <nicolas.jacquemin26@gmail.com>
2026-02-23 13:35:24 +0100

    Add 4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a message with puts
+ * Return: 0 if program run
+ */
+
+int main(void)
+{
+puts "Programming is like building a multilingual puzzle\n";
+return(0);
+}
