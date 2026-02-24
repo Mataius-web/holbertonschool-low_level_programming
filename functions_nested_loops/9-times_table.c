332586a4bf6f4add30bdc66ca2de8c35d92e716e
Nicolas <nicolas.jacquemin26@gmail.com>
2026-02-24 22:39:26 +0100

    Add 9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -20,17 +20,19 @@ if (j != 0)
 {
 _putchar(',');
 _putchar(' ');
+
+if (result < 10)
+_putchar(' ');
 }
 
-if (result < 10 && j != 0)
-{
-_putchar(' ');
-_putchar(result + '0');
-}
-else
+if (result >= 10)
 {
 _putchar(result / 10 + '0');
 _putchar(result % 10 + '0');
+}
+else
+{
+_putchar(result + '0');
 }
 }
 _putchar('\n');
