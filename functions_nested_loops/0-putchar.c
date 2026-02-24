f4b8a3e8f395aca34ea1be56553e2bc52ba62e7d
Nicolas <nicolas.jacquemin26@gmail.com>
2026-02-24 13:21:51 +0100

    Add 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - print _putchar with _putchar
  * Return: 0 if run
  */
 
-int _putchar(void)
+int main(void)
 {
-_putchar("_putchar");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
