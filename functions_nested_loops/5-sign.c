9c2ac63fe5b8c417e68ee852781b63981429dc90
Nicolas <nicolas.jacquemin26@gmail.com>
2026-02-24 16:20:58 +0100

    Add 5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of n
+ * @n: number
+ * Return: 1 if + and n greater than 0
+ * 0 is n is 0 -1 if - and n less than 0
+ */
+
+int print_sign(int n)
+{
+int n;
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+return (0);
+{
+_putchar('0');
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
