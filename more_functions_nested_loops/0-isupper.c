509b4bf2bf4fb4bd4a5dcda900477f7e1a556c44
Nicolas <nicolas.jacquemin26@gmail.com>
2026-02-25 13:57:11 +0100

    Add 0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - check if lowercase or uppercase
+ * @c: letter
+ * Return: 1 if uppercase 0 if else
+ */
+
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
