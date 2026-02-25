dec87935065d055f3f920582327899a7f5f5e82d
Nicolas <nicolas.jacquemin26@gmail.com>
2026-02-25 21:11:19 +0100

    Add 1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if c is a number from 0 to 9
+ * @c: number
+ * Return: 1 if it's a digit and 0 if not
+ */
+
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
