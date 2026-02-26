137e27ca6dc235455510d6d2c5ef6afee4cb8a54
Nicolas <nicolas.jacquemin26@gmail.com>
2026-02-26 15:45:55 +0100

    Add 9-fizz_buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz - fizz buzz function
+ */
+
+void fizz_buzz(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+printf("\n");
+}
