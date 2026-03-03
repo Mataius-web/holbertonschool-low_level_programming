e7fc22bd69034590bea06bfb375d91c128c551a4
Nicolas <nicolas.jacquemin26@gmail.com>
2026-03-03 14:06:15 +0100

    Add 5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string to reverse
+ */
+
+void rev_string(char *s)
+{
+int start;
+int end;
+char tempo;
+
+start = 0;
+end = 0;
+while (s[end] != '\0')
+end++;
+end--;
+while (start < end)
+{
+temp = s[start];
+s[start] = s[end];
+s[end] = tempo;
+start++;
+end--;
+}
+}
