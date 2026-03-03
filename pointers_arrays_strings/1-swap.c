36d2aff5f420711373f00a24828fd55dc80dd8de
Nicolas <nicolas.jacquemin26@gmail.com>
2026-03-03 08:47:12 +0100

    Add 1-swap_int.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *swap_int - swap value of 2 variables int
+ * @a: first variable
+ * @b: second variable
+ */
+
+void swap_int(int *a, int *b)
+{
+int swap = *a;
+*a = *b;
+*b = swap;
+}
