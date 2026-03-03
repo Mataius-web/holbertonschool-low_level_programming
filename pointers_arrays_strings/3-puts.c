3afb2760f0978958054702dea15df4366a070383
Nicolas <nicolas.jacquemin26@gmail.com>
2026-03-03 13:20:38 +0100

    Add 3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - print string char with new line
+ * @str: string to print
+ */
+
+
+void _puts(char *str)
+{
+
+int i;
+
+i = 0;
+while (*str[i] != '\0')
+{
+_putchar(str[i]);
+i++
+}
+_putchar('\n');
+}
