bf0c02c0c532845b296380a24b8fe765b89358c7
Nicolas <nicolas.jacquemin26@gmail.com>
2026-03-04 22:00:22 +0100

    Add 2-strchr.c && git push
    git add 2-strchr.c && git commit -m Add

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string to search in
+ * @c: the character to find
+ * Return: pointer to first occurrence of c, or NULL if not found
+ */
+
+
+char *_strchr(char *s, char c)
+{
+unsigned int i;
+
+i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+return (&s[i]);
+i++;
+}
+if (c == '\0')
+return (&s[i]);
+return (NULL);
+}
