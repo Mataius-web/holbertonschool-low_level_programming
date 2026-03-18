760ae547cb63dedd3b4e28130dc6d8c0036391dd
Nicolas <nicolas.jacquemin26@gmail.com>
2026-03-18 11:22:18 +0100

    MAJ

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,63 +1,57 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the new string, or NULL on failure
+ */
+static char *_strdup(char *str)
+{
+	char *copy;
+	int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[len])
+		len++;
+	copy = malloc(sizeof(char) * (len + 1));
+	if (!copy)
+		return (NULL);
+	memcpy(copy, str, len + 1);
+	return (copy);
+}
 
 /**
  * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
  *
- * Return: pointer to new dog, or NULL if it fails
+ * Return: pointer to the new dog, or NULL on failure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int i, len_name = 0, len_owner = 0;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
-	/* calculate length of name */
-	for (i = 0; name[i]; i++)
-		len_name++;
-
-	/* calculate length of owner */
-	for (i = 0; owner[i]; i++)
-		len_owner++;
-
-	/* allocate memory for dog struct */
 	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	if (!dog)
 		return (NULL);
-
-	/* allocate memory for name */
-	dog->name = malloc(len_name + 1);
-	if (dog->name == NULL)
+	dog->name = _strdup(name);
+	if (!dog->name)
 	{
 		free(dog);
 		return (NULL);
 	}
-
-	/* allocate memory for owner */
-	dog->owner = malloc(len_owner + 1);
-	if (dog->owner == NULL)
+	dog->owner = _strdup(owner);
+	if (!dog->owner)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-
-	/* copy name */
-	for (i = 0; i < len_name; i++)
-		dog->name[i] = name[i];
-	dog->name[len_name] = '\0';
-
-	/* copy owner */
-	for (i = 0; i < len_owner; i++)
-		dog->owner[i] = owner[i];
-	dog->owner[len_owner] = '\0';
-
 	dog->age = age;
-
 	return (dog);
 }
