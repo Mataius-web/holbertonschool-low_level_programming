881c1bc172e014896ef274fbce1ed924522a241f
Mataius-web <nicolas.jacquemin26@gmail.com>
2026-04-01 11:46:25 +0200

    Add 1st task

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -16,11 +16,27 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+/* singly linked list functions */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
+/* doubly linked list function */
 size_t print_dlistint(const dlistint_t *h);
+
 #endif
