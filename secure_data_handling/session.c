eaf7e5694e709fae41dc961f04b3d1ad55daa422
Mataius-web <nicolas.jacquemin26@gmail.com>
2026-04-02 01:44:07 +0200

    Add all files

--- secure_data_handling/session.c
+++ secure_data_handling/session.c
@@ -2,45 +2,38 @@
 #include <string.h>
 #include "session.h"
 
-/**
- * session_create - create a new session safely
- * @id: session identifier (string)
- * @uid: user id
- * @data: pointer to data buffer
- * @data_len: length of data buffer
- *
- * Return: pointer to newly created session, or NULL on failure
- */
-
-session_t *session_create(const char *id, unsigned int uid,
-						  const unsigned char *data, size_t data_len)
+session_t *session_create(const char *id, unsigned int uid, const unsigned char *data, size_t data_len)
 {
 	session_t *s;
 
-	if (!id)
-		return (NULL);
-
-	s = malloc(sizeof(*s));
+	s = (session_t *)malloc(sizeof(*s));
 	if (!s)
-		return (NULL);
+		return NULL;
 
-	s->id = strdup(id);
-	if (!s->id)
+	if (id)
 	{
-		free(s);
-		return (NULL);
+		s->id = strdup(id);
+		if (!s->id)
+		{
+			free(s);
+			return NULL;
+		}
+	}
+	else
+	{
+		s->id = NULL;
 	}
 
 	s->uid = uid;
 
 	if (data_len > 0)
 	{
-		s->data = malloc(data_len);
+		s->data = (unsigned char *)malloc(data_len);
 		if (!s->data)
 		{
 			free(s->id);
 			free(s);
-			return (NULL);
+			return NULL;
 		}
 		memcpy(s->data, data, data_len);
 		s->data_len = data_len;
@@ -51,55 +44,47 @@ session_t *session_create(const char *id, unsigned int uid,
 		s->data_len = 0;
 	}
 
-	return (s);
+	return s;
 }
-
-/**
- * session_set_data - safely set or update session data
- * @s: pointer to session
- * @data: pointer to new data
- * @data_len: length of new data
- *
- * Return: 1 on success, 0 on failure
- */
 
 int session_set_data(session_t *s, const unsigned char *data, size_t data_len)
 {
 	unsigned char *tmp;
 
 	if (!s)
-		return (0);
+		return 0;
 
 	if (data_len == 0)
 	{
-		free(s->data);
-		s->data = NULL;
+		if (s->data)
+		{
+			free(s->data);
+			s->data = NULL;
+		}
 		s->data_len = 0;
-		return (1);
+		return 1;
 	}
 
-	tmp = realloc(s->data, data_len);
+	tmp = (unsigned char *)realloc(s->data, data_len);
 	if (!tmp)
-		return (0);
-
+	{
+		return 0;
+	}
 	s->data = tmp;
 	memcpy(s->data, data, data_len);
 	s->data_len = data_len;
-
-	return (1);
+	return 1;
 }
-
-/**
- * session_destroy - safely free a session
- * @s: pointer to session
- */
 
 void session_destroy(session_t *s)
 {
 	if (!s)
 		return;
 
-	free(s->id);
-	free(s->data);
+	if (s->id)
+		free(s->id);
+
+	if (s->data)
+		free(s->data);
 	free(s);
 }
