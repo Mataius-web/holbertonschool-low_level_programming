f37728d6d7116a38b90a94c4775df03eb05e8df2
Mataius-web <nicolas.jacquemin26@gmail.com>
2026-04-10 12:51:11 +0200

    Add 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,11 +9,110 @@
  * Return: 0 on success
  */
 
+/**
+ * close_or_die - Closes a file descriptor or exits with code 100.
+ * @fd: File descriptor to close.
+ */
+
+void close_or_die(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * open_from_file - Opens source file for reading.
+ * @filename: Source file name.
+ *
+ * Return: Open file descriptor.
+ */
+
+int open_from_file(char *filename)
+{
+	int fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	return (fd);
+}
+
+/**
+ * open_to_file - Opens destination file for writing.
+ * @filename: Destination file name.
+ * @fd_from: Source file descriptor to close on error.
+ *
+ * Return: Open file descriptor.
+ */
+
+int open_to_file(char *filename, int fd_from)
+{
+	int fd;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		close_or_die(fd_from);
+		exit(99);
+	}
+	return (fd);
+}
+
+/**
+ * copy_file_content - Copies bytes from one fd to another.
+ * @fd_from: Source file descriptor.
+ * @fd_to: Destination file descriptor.
+ * @to_name: Destination file name for error display.
+ * @from_name: Source file name for error display.
+ */
+
+void copy_file_content(int fd_from, int fd_to, char *to_name, char *from_name)
+{
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+	char buffer[1024];
+
+	bytes_read = read(fd_from, buffer, 1024);
+	while (bytes_read > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to_name);
+			close_or_die(fd_from);
+			close_or_die(fd_to);
+			exit(99);
+		}
+		bytes_read = read(fd_from, buffer, 1024);
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_name);
+		close_or_die(fd_from);
+		close_or_die(fd_to);
+		exit(98);
+	}
+}
+
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: Number of arguments.
+ * @argv: Arguments array.
+ *
+ * Return: Always 0 on success.
+ */
+
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-	ssize_t r, w;
-	char buffer[1024];
+	int fd_from;
+	int fd_to;
 
 	if (argc != 3)
 	{
@@ -21,59 +120,11 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
-		exit(99);
-	}
-
-	while ((r = read(fd_from, buffer, 1024)) > 0)
-	{
-		w = write(fd_to, buffer, r);
-		if (w != r)
-		{
-			dprintf(STDERR_FILENO,
-					"Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
-		}
-	}
-
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
-		exit(98);
-	}
-
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO,
-				"Error: Can't close fd %d\n", fd_from);
-		close(fd_to);
-		exit(100);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO,
-				"Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+	fd_from = open_from_file(argv[1]);
+	fd_to = open_to_file(argv[2], fd_from);
+	copy_file_content(fd_from, fd_to, argv[2], argv[1]);
+	close_or_die(fd_from);
+	close_or_die(fd_to);
 
 	return (0);
 }
