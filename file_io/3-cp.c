6f524ae2d219663a6002ed55da2e934a9a2404ec
Mataius-web <nicolas.jacquemin26@gmail.com>
2026-04-10 12:46:48 +0200

    Add 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,8 +3,8 @@
 
 /**
  * main - copies content of a file to another file
- * @argc: argument count
- * @argv: argument vector
+ * @argc: number of arguments
+ * @argv: arguments vector
  *
  * Return: 0 on success
  */
@@ -12,26 +12,28 @@
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
-	ssize_t r, w, c;
+	ssize_t r, w;
 	char buffer[1024];
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
 		exit(99);
 	}
@@ -41,7 +43,8 @@ int main(int argc, char *argv[])
 		w = write(fd_to, buffer, r);
 		if (w != r)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO,
+					"Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
@@ -50,24 +53,25 @@ int main(int argc, char *argv[])
 
 	if (r == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
 		exit(98);
 	}
 
-	c = close(fd_from);
-	if (c == -1)
+	if (close(fd_from) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		dprintf(STDERR_FILENO,
+				"Error: Can't close fd %d\n", fd_from);
 		close(fd_to);
 		exit(100);
 	}
 
-	c = close(fd_to);
-	if (c == -1)
+	if (close(fd_to) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
+		dprintf(STDERR_FILENO,
+				"Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
 
